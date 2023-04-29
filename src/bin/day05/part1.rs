20719b4408947495e01f39106c842641192a7794
valentinRog <valentin.rogister@gmail.com>
2023-04-29 23:17:09 +0200

    day05

---
+++ src/bin/day05/part1.rs
@@ -0,0 +1,15 @@
+pub fn solve(input: &str) {
+    let mut v: Vec<_> = input
+        .split("\n")
+        .map(|x| x.parse::<i32>().unwrap())
+        .collect();
+    let mut i: i32 = 0;
+    let mut k = 0;
+    while i >= 0 && i < v.len() as i32 {
+        let n = &mut v[i as usize];
+        i += *n;
+        *n += 1;
+        k += 1;
+    }
+    println!("{k}");
+}

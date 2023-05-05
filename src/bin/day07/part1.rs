b6e22cc6a77c7822613dd68828aea3690faecfa0
valentinRog <valentin.rogister@gmail.com>
2023-05-06 00:40:48 +0200

    day07 part1

---
+++ src/bin/day07/part1.rs
@@ -0,0 +1,16 @@
+use std::collections::HashSet;
+
+pub fn solve(input: &str) {
+    let st = input
+        .split("\n")
+        .filter(|x| x.contains("->"))
+        .map(|x| x.split("->").nth(1).unwrap())
+        .flat_map(|x| x.split(",").map(|x| x.trim()))
+        .collect::<HashSet<_>>();
+    let res = input
+        .split("\n")
+        .map(|x| x.split_whitespace().nth(0).unwrap())
+        .find(|x| st.get(x).is_none())
+        .unwrap();
+    println!("{res}");
+}

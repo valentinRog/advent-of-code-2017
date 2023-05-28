ad6780d7d451b93627a14335792c9c1bc1b810b6
valentinRog <valentin.rogister@gmail.com>
2023-05-28 12:50:57 +0200

    day18 part2

--- src/bin/day18/part2.rs
+++ src/bin/day18/part2.rs
@@ -1,48 +1,58 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 
 fn get_val(k: &str, rs: &HashMap<&str, i64>) -> i64 {
     k.parse::<i64>().unwrap_or(*rs.get(k).unwrap_or(&0))
 }
 
 pub fn solve(input: &str) {
-    let mut i = 0i32;
     let lines = input.lines().collect::<Vec<_>>();
-    let mut rs = HashMap::new();
-    let mut last_sound = 0;
-    while i >= 0 && i < lines.len() as i32 {
-        let mut arr = lines[i as usize].split_whitespace();
-        let ins = arr.next().unwrap();
-        let x = arr.next().unwrap();
-        let y = arr.next();
-        match ins {
-            "snd" => {
-                last_sound = get_val(x, &rs);
-            }
-            "set" => {
-                rs.insert(x, get_val(y.unwrap(), &rs));
-            }
-            "add" => {
-                rs.insert(x, rs.get(x).unwrap_or(&0) + get_val(y.unwrap(), &rs));
-            }
-            "mul" => {
-                rs.insert(x, rs.get(x).unwrap_or(&0) * get_val(y.unwrap(), &rs));
-            }
-            "mod" => {
-                rs.insert(x, rs.get(x).unwrap_or(&0) % get_val(y.unwrap(), &rs));
-            }
-            "rcv" => {
-                if last_sound != 0 {
-                    println!("{last_sound}");
-                    break;
+    let mut idx = [0i32, 0];
+    let mut rs = [HashMap::new(), HashMap::from([("p", 1)])];
+    let mut qs = [VecDeque::new(), VecDeque::new()];
+    let mut locks = [false, false];
+    let mut res = 0;
+    while !locks.iter().all(|x| *x) {
+        for i in 0..2 {
+            let mut arr = lines[idx[i] as usize].split_whitespace();
+            let ins = arr.next().unwrap();
+            let x = arr.next().unwrap();
+            let y = arr.next();
+            match ins {
+                "snd" => {
+                    qs[(i + 1) % 2].push_back(get_val(x, &rs[i]));
+                    if i == 1 {
+                        res += 1;
+                    }
                 }
-            }
-            "jgz" => {
-                if get_val(x, &rs) > 0 {
-                    i += (get_val(y.unwrap(), &rs) - 1) as i32;
+                "set" => {
+                    rs[i].insert(x, get_val(y.unwrap(), &rs[i]));
                 }
+                "add" => {
+                    rs[i].insert(x, rs[i].get(x).unwrap_or(&0) + get_val(y.unwrap(), &rs[i]));
+                }
+                "mul" => {
+                    rs[i].insert(x, rs[i].get(x).unwrap_or(&0) * get_val(y.unwrap(), &rs[i]));
+                }
+                "mod" => {
+                    rs[i].insert(x, rs[i].get(x).unwrap_or(&0) % get_val(y.unwrap(), &rs[i]));
+                }
+                "rcv" => {
+                    if let Some(val) = qs[i].pop_front() {
+                        rs[i].insert(x, val);
+                    } else {
+                        locks[i] = true;
+                        continue;
+                    }
+                }
+                "jgz" => {
+                    if get_val(x, &rs[i]) > 0 {
+                        idx[i] += (get_val(y.unwrap(), &rs[i]) - 1) as i32;
+                    }
+                }
+                _ => panic!(),
             }
-            _ => panic!(),
+            idx[i] += 1;
         }
-        i += 1;
     }
+    println!("{res}");
 }

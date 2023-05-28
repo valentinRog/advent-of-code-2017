269a8ca7e3a57b86707a3201c40bd5eb5b9cd362
valentinRog <valentin.rogister@gmail.com>
2023-05-28 13:42:12 +0200

    day19

---
+++ src/bin/day19/part2.rs
@@ -0,0 +1,51 @@
+#[derive(Debug, Eq, PartialEq, Clone, Copy)]
+struct Complex {
+    r: i32,
+    i: i32,
+}
+
+impl std::ops::Add for Complex {
+    type Output = Complex;
+    fn add(self, rhs: Complex) -> Complex {
+        Complex {
+            r: self.r + rhs.r,
+            i: self.i + rhs.i,
+        }
+    }
+}
+
+impl std::ops::Mul for Complex {
+    type Output = Complex;
+    fn mul(self, rhs: Complex) -> Complex {
+        Complex {
+            r: self.r * rhs.r - self.i * rhs.i,
+            i: self.r * rhs.i + self.i * rhs.r,
+        }
+    }
+}
+
+pub fn solve(input: &str) {
+    let m = input
+        .lines()
+        .map(|x| x.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+    let mut d = Complex { r: 0, i: 1 };
+    let mut p = Complex { r: 0, i: 0 };
+    p.r = m[0].iter().enumerate().find(|(_, x)| **x == '|').unwrap().0 as i32;
+    for i in 0.. {
+        match m[p.i as usize][p.r as usize] {
+            ' ' => {
+                println!("{i}");
+                break;
+            }
+            '+' => {
+                d = *[d * Complex { r: 0, i: 1 }, d * Complex { r: 0, i: -1 }]
+                    .iter()
+                    .find(|x| m[(p.i + x.i) as usize][(p.r + x.r) as usize] != ' ')
+                    .unwrap();
+            }
+            _ => {}
+        }
+        p = p + d;
+    }
+}
